6e0bcded7248bd63ad4c3aaf03ee8db41e789e77
ChenPengfei <pengfei@zhiyitech.cn>
2024-04-01 14:18:59 +0800

    调整动态库命名；根据苹果官方工作人员在论坛回复，无隐私Api时，不需要增加空的隐私清单文件

--- Package.swift
+++ Package.swift
@@ -9,25 +9,26 @@ let package = Package(
             name: "CPFListController",
             targets: ["CPFListController"]),
         .library(
-            name: "CPFListController-Dynamic",
+            name: "CPFListControllerDynamic",
             type: .dynamic,
             targets: ["CPFListController"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Loadar/CPFChain.git", from: Version(stringLiteral: "2.2.5")),
+        .package(url: "https://github.com/Loadar/CPFChain.git", from: Version(stringLiteral: "2.2.6")),
     ],
     targets: [
         .target(
-            name: "CPFListController-Foundation",
+            name: "CPFListControllerFoundation",
             dependencies: [],
-            path: "Sources/CPFListController/Foundation",
-            resources: [.copy("../../PrivacyInfo.xcprivacy")]
+            path: "Sources/CPFListController/Foundation"
         ),
         .target(
             name: "CPFListController",
-            dependencies: ["CPFChain", "CPFListController-Foundation"],
-            path: "Sources/CPFListController/Interface",
-            resources: [.copy("../../PrivacyInfo.xcprivacy")]
+            dependencies: [
+                .product(name: "CPFChainDynamic", package: "CPFChain"),
+                "CPFListControllerFoundation"
+            ],
+            path: "Sources/CPFListController/Interface"
         ),
     ]
 )
